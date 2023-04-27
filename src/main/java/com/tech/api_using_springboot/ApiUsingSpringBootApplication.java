979e7bbe31a67b69f0925a1eeb01cffb1ccf47d7
Syed Sanzid Hasan <syed.hasan@nagad.com.bd>
2023-04-27 16:54:17 +0600

    Commit before adding new functionalities

--- src/main/java/com/tech/api_using_springboot/ApiUsingSpringBootApplication.java
+++ src/main/java/com/tech/api_using_springboot/ApiUsingSpringBootApplication.java
@@ -2,11 +2,12 @@ package com.tech.api_using_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ApiUsingSpringBootApplication {
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args){
         SpringApplication.run(ApiUsingSpringBootApplication.class, args);
     }
 
